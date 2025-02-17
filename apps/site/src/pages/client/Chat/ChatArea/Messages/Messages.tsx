b70da37fed34065f137d6c51184370fbc6777fdf
nick_romanov <nikolay.romanov164@gmail.com>
2025-02-17 19:51:19 +0300

    Fixed deploy file

--- apps/site/src/pages/client/Chat/ChatArea/Messages/Messages.tsx
+++ apps/site/src/pages/client/Chat/ChatArea/Messages/Messages.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 
-// import { Virtuoso } from "react-virtuoso"
-
 import { Message } from "../Message"
 import styles from "./Messages.module.scss"
 import { store } from "@/store"
@@ -31,7 +29,6 @@ export function Messages() {
 
   const [messages, setMessages] = React.useState<Message[]>([])
   // const [page, setPage] = React.useState(0)
-  // const virtuosoRef = React.useRef<any>(null)
 
   React.useEffect(() => {
     socket?.addEventListener("message", (event) => {
@@ -58,61 +55,11 @@ export function Messages() {
           break
       }
     })
-    // socket?.onmessage?.((event: MessageEvent) => {
-    //   debugger
-    //   const message = event.data as Message
-    //   setMessages((prev) => [...prev, message]);
-    // })
-    // loadMoreMessages(page, true) // Изначальная загрузка сообщений
   }, [socket])
-  // const loadMoreMessages = (page: number, initialLoad = false) => {
-  //   // Имитируем загрузку старых сообщений
-  //   const newMessages = Array.from(
-  //     { length: 20 },
-  //     (_, i) => `Message ${i + page * 20}`,
-  //   )
 
-  //   setMessages((prev) => [...newMessages, ...prev])
-
-  //   if (initialLoad) {
-  //     // Установка позиции на последний элемент при первой загрузке
-  //     setTimeout(() => {
-  //       virtuosoRef.current?.scrollToIndex(newMessages.length - 1)
-  //     }, 0)
-  //   } else {
-  //     // Фиксация позиции после подгрузки новых сообщений
-  //     setTimeout(() => {
-  //       virtuosoRef.current?.scrollToIndex(newMessages.length)
-  //     }, 0)
-  //   }
-  // }
   return (
     <div className={styles.base}>
-      {/* <div className={styles.noMessageText}>
-        У вас пока нет ни одного сообщения.
-      </div> */}
       <div className={styles.messages}>
-        {/* <Virtuoso
-          ref={virtuosoRef}
-          data={messages}
-          // initialLocation={{ index: 'LAST', align: 'end' }}
-          firstItemIndex={0}
-          startReached={() => console.log("234")}
-          itemContent={(_, message) => (
-            <Message
-              key={message.id}
-              id={message.id}
-              content={message.content}
-              created={message.createdAt}
-              userName={message.userName}
-              isOwner={message.userId === user?.id}
-            />
-          )}
-          // endReached={loadMore}
-          //     increaseViewportBy={200}
-          // components={{ Footer }}
-          // style={{ height: "500px" }} // Ограничение высоты контейнера
-        /> */}
         {__.map(messages, (message) => {
           return (
             <Message
